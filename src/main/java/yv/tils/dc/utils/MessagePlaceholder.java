8de3570144fff1232f0f663fb6b3e2dc96d59b73
WolfiiYV <minecraftwandler@gmail.com>
2024-12-04 23:07:15 +0100

    Remove unneded files

--- src/main/java/yv/tils/dc/utils/MessagePlaceholder.java
+++
@@ -1,7 +0,0 @@
-package yv.tils.dc.utils;
-
-public class MessagePlaceholder {
-    public static String PREFIXUPDATE = "§c[YVtils-UPDATE]";
-    public static String PREFIXNOUPDATE = "§7[YVtils-UPDATE]";
-    public static String PREFIXDC = "§d[YVtils-DC]";
-}
